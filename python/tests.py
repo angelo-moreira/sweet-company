f873a669379b80d720de2519384d75672c5c9118
Angelo Moreira <angelo.m@designone.co.uk>
2020-05-22 15:59:42 +0100

    found an edge case

--- python/tests.py
+++ python/tests.py
@@ -11,6 +11,10 @@ right_ans = [250]
 ans = solution.order(250)
 print(right_ans == ans)
 
+right_ans = [1000]
+ans = solution.order(1000)
+print(right_ans == ans)
+
 right_ans = [500]
 ans = solution.order(251)
 print(right_ans == ans)
@@ -28,8 +32,8 @@ ans = solution.order(0)
 print(right_ans == ans)
 
 
-new_packs = [1, 500, 1000, 2000, 5000]
-solution = SweetCompany(new_packs)
+packs = [1, 500, 1000, 2000, 5000]
+solution = SweetCompany(packs)
 
 right_ans = [1, 1, 1, 1, 1, 1]
 ans = solution.order(6)
@@ -42,4 +46,12 @@ print(right_ans == ans)
 right_ans = [5000, 5000, 5000, 5000, 5000, 5000,
              5000, 5000, 5000, 2000, 1000, 1, 1, 1, 1]
 ans = solution.order(48004)
+print(right_ans == ans)
+
+
+packs = [10, 20, 200, 500]
+solution = SweetCompany(packs)
+
+right_ans = [200, 200, 200]
+ans = solution.order(600)
 print(right_ans == ans)
